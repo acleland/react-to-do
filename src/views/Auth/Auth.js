968e1653a0cfbf02e50ac4586f35cf76ad05ad8f
Andrea Cleland <andreaclelan@gmail.com>
2022-04-01 10:32:12 -0700

    user can now toggle auth state between sign in and sign up

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useState } from 'react';
+import './Auth.css';
 
 export default function Auth() {
   const [isSignUp, setIsSignUp] = useState(true);
 
+  const toggleAuth = () => {
+    setIsSignUp(!isSignUp);
+  };
+
   return (
     <div>
       <div className="auth-menu">
-        <span className="auth-toggle">Sign In</span>
-        <span className="auth-toggle selected">Sign Up</span>
+        <span className={isSignUp ? 'auth-toggle' : 'auth-toggle selected'} onClick={toggleAuth}>
+          Sign In
+        </span>
+        <span className={isSignUp ? 'auth-toggle selected' : 'auth-toggle'} onClick={toggleAuth}>
+          Sign Up
+        </span>
       </div>
     </div>
   );
